292c1bc53deae4b698953d0147b2565edd1aa908
rbasa <=>
2023-01-07 14:46:45 -0300

    use number format for port

--- database/config/config.js
+++ database/config/config.js
@@ -12,7 +12,7 @@ module.exports = {
     "database": process.env.DBDB||secrets.db,
     "host": process.env.DBHOSTNAME||secrets.hostname,
     "dialect": "mysql",
-    "port": process.env.PORTDB||secrets.port
+    "port": secrets.port||3306
   },
   "test": {
     "username": "root",
