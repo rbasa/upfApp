aa15f1fea0f1339bdc518bef87a10730810ce814
rbasa <=>
2023-01-10 11:59:02 -0300

    indent file

--- src/middlewares/privateFiles.js
+++ src/middlewares/privateFiles.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const userLogged = require('./userLogged')
+
 function checkPermissions(req, res, next) {
   if (!req.session || !req.session.userLogged) {
     return res.status(403).send('Forbidden');
   }
-
   const userCategoryId = req.session.userLogged.user_category_id;
   switch (userCategoryId) {
     case 3:
@@ -12,14 +12,14 @@ function checkPermissions(req, res, next) {
       if (req.session.userLogged.registered === 1) {
         return next();
       }
-      break;
+    break;
     case 2:
       const userId = req.session.userLogged.user_id;
       const fileUserId = Number(req.path.split('/')[2]);
       if (userId === fileUserId) {
         return next();
       }
-      break;
+    break;
   }
 
   res.status(403).send('Forbidden');
