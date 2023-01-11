e9921d49835e22d009fdeccacc3c0d499f97b5e0
rbasa <=>
2023-01-11 16:29:59 -0300

    repair upload midleware

--- src/middlewares/mintingUpload.js
+++ src/middlewares/mintingUpload.js
@@ -1,6 +1,6 @@
 const multer = require('multer');
 const path = require('path');
-const fs =require('fs')
+const fs = require('fs');
 const crypto = require('crypto');
 
 const counters = {
@@ -14,31 +14,34 @@ const counters = {
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     const userId = req.session.userLogged.user_id;
+    let dir = path.join(__dirname, '..', 'private', 'enterpriseDocumentation');
+
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+    dir = path.join(dir, userId);
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
     if(!req.dirName){
-      const dirName = crypto.randomBytes(8).toString('hex')
+      const dirName = crypto.randomBytes(8).toString('hex');
       req.dirName = dirName;
     }
-    let dir = path.join('../private/enterpriseDocumentation');
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir);
-    }
-    dir = path.join(dir,`/${userId}`)
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir);
-    }
-    dir = path.join(dir,`/${req.dirName}`)
+
+    dir = path.join(dir, req.dirName);
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir);
     }
     cb(null, dir);
   },
   filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
     const field = file.fieldname;
     counters[field] += 1;
-    cb(null, file.fieldname + '-' + counters[field] + '-' + uniqueSuffix + path.extname(file.originalname))
+    cb(null, `${file.fieldname}-${counters[field]}-${uniqueSuffix}${path.extname(file.originalname)}`);
   }
-})
+});
+
 const upload = multer({ storage });
 
 module.exports = upload;
