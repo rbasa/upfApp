15ad2daeec555c2c7487ada08305f3f0572c77b2
rbasa <=>
2023-03-07 12:22:56 -0300

    desestructure properly sequelize objects

--- src/middlewares/mintingRequestEdit.js
+++ src/middlewares/mintingRequestEdit.js
@@ -4,12 +4,12 @@ const Minting_request = require('../models/Minting_Request');
 async function editPermision(req, res, next){
   // Unplastify can allways edit Minting Request
   if(req.session.userLogged.user_category_id==3){
+    console.log('sucede esto')
     return next();
   }
-  const statusNotAllowed = await Minting_request.getStatus('Submited');
+  const { id_status: statusNotAllowed } = await Minting_request.getStatus('Submited');
   const { minting_request_id: idMintingRequest } = req.params.minting_request_id || await Unplastified_item.findMintingRequest(req.params.idUnplastifiedItem)
-  const mintingRequestStatus = await Minting_request.findMintingRequestStatus(idMintingRequest);
-  
+  const { status_id: mintingRequestStatus } = await Minting_request.findMintingRequestStatus(idMintingRequest);
   // Submited MR should not be edited
   if(statusNotAllowed == mintingRequestStatus){
     return res.redirect('/enterprise/');
