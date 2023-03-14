08de4e4738a643ec368d594347eee0b93934b46b
rbasa <=>
2023-03-14 19:57:48 -0300

    add chat routing

---
+++ src/routes/chatRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const chat = require('../models/Chat')
+const userNotLogged = require('../middlewares/userNotLogged');
+
+// build middleware to make enterprice chat only on its mintingRequests
+// build middleware to make validators chat only on its mintingRequests
+// build chat model
+
+router.post('/', userNotLogged, async (req, res)=>{
+  console.log('***********************************')
+  console.log('***********************************')
+  console.log('***********************************')
+  console.log('***********************************')
+  await chat.createNewMsg(req)
+  return res.redirect(`/enterprise/mintingRequest/${req.session.minting_request_id}`)
+});
+
+module.exports = router;
