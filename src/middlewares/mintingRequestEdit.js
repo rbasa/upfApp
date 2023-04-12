f87c534d944d02f4ec8120875306db58c5c80893
rbasa <=>
2023-04-11 22:19:46 -0300

    delete unneccesary console.log()

--- src/middlewares/mintingRequestEdit.js
+++ src/middlewares/mintingRequestEdit.js
@@ -4,7 +4,6 @@ const Minting_request = require('../models/Minting_Request');
 async function editPermision(req, res, next){
   // Unplastify can allways edit Minting Request
   if(req.session.userLogged.user_category_id==3){
-    console.log('sucede esto')
     return next();
   }
   const { id_status: statusNotAllowed } = await Minting_request.getStatus('Submited');
