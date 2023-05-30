3559ddd4bdf75720491817e95b17073649b2f9e8
rbasa <=>
2023-05-30 17:06:34 -0300

    remove universal redirections from main api endpoint

--- src/routes/mainRoutesApi.js
+++ src/routes/mainRoutesApi.js
@@ -3,7 +3,5 @@ const router = express.Router();
 const controller = require('../controllers/mainControllerApi');
 
 router.get('/api/', controller.index);
-router.get('*', (req, res) => {
-  res.redirect('/');
-});
+
 module.exports = router;
