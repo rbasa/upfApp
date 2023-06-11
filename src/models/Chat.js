ee1ea8a47bb0318ca2238c7bc622af806eb14cc5
rbasa <=>
2023-06-11 20:55:30 -0300

    use sequelize query

--- src/models/Chat.js
+++ src/models/Chat.js
@@ -1,13 +1,32 @@
 const db = require("../../database/models");
 
 const Chat = {
-  createNewMsg: async function(req) {
-    const newMintingRequest = await db.ChatRoom.create({
-      minting_request_id: req.session.minting_request_id,
-      user_category_id: req.session.userLogged.user_category_id,
-      msg: req.body.newMsg
-    });
-    return newMintingRequest.minting_request_id;
+  createNewMsg: async function(req, decodedToken) {
+    return await db.sequelize.query(
+      `
+      INSERT INTO
+        chat_room (minting_request_id, user_category_id, msg)
+        SELECT 
+          :mintingRequestId,
+          (
+            SELECT
+              id
+            FROM
+              user_category
+            WHERE
+              user_category = :userCategory
+          ),
+          :newMsg;
+      `,
+      {
+        replacements: {
+          mintingRequestId: parseInt(req.session.minting_request_id),
+          userCategory: decodedToken.userCategory,
+          newMsg: req.body.newMsg
+        },
+        type: db.Sequelize.QueryTypes.INSERT
+      }
+    );
   },
   findByMintingRequest: async (user_id) => {
     return await db.ChatRoom.findAll({
