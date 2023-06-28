885c81f9ed689d90dcb2622050ddea25e5b4484d
rbasa <=>
2023-06-28 14:00:12 -0300

    remove unnecesary middlewares

--- src/middlewares/userNotLoggedAsUnplastify.js
+++
@@ -1,7 +0,0 @@
-function userMiddleware(req, res, next){
-  if(!req.session.userLogged||req.session.userLogged.user_category_id!==3){
-    return res.redirect('/users/login');
-  };
-  next();
-}
-module.exports = userMiddleware;
