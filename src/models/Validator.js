d3d427caf04684f409e7d95495a35fa6d17e4bf3
rbasa <=>
2023-09-20 17:18:13 -0300

    handle cancelation validation cases

--- src/models/Validator.js
+++ src/models/Validator.js
@@ -100,6 +100,75 @@ const Validator = {
       }
     );
   },
+  replaceValidator: async function (mintingRequestId) {
+    console.log(`Replacing Validator minting request ${mintingRequestId} `)
+    const result = await db.sequelize.query(
+      `
+      SELECT
+        a.user_id,
+        COALESCE(b.assigned_count, 0) AS assigned_count
+      FROM
+        user a
+      INNER JOIN
+        (
+          SELECT
+            c.*,
+            (
+              SELECT
+                COUNT(*)
+              FROM
+                validation_tracking
+              WHERE
+                user_id = c.user_id
+              AND
+                validation_status_id = (
+                  SELECT
+                    validation_status_id
+                  FROM
+                    validation_status
+                  WHERE
+                    status = 'Assigned'
+                )
+            ) AS assigned_count
+            FROM
+              validation_tracking c
+            WHERE
+              user_id not in
+                (
+                  select
+                    user_id
+                  from
+                    validation_tracking
+                  where
+                    minting_request_id = :mintingRequestId
+                )
+        ) b
+      ON
+        b.user_id = a.user_id
+      WHERE
+        user_category_id = (
+          SELECT
+            id
+          FROM
+            user_category
+          WHERE
+            user_category = 'validator'
+        )
+        AND registered = 1
+      ORDER BY
+        assigned_count ASC, RAND()
+      LIMIT 1;
+      `, { replacements: { mintingRequestId }, type: db.Sequelize.QueryTypes.SELECT }
+    );
+    console.log(`Replace complete`)
+    const newValidator = result[0] ? result[0].user_id : null;
+    console.log('The new Validator its ', newValidator)
+    if (newValidator) {
+      console.log(`Re asigning validator ${mintingRequestId} newValidator ${newValidator}`)
+      await this.assignValidator(mintingRequestId, newValidator, 'Assigned')
+    }
+    return
+  },
   obtainAllStatus: async function (minting_request_id) {
     return await db.sequelize.query(
       `
