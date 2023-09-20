6b24d75a53c089652ee6378f32b9e942d3b34ad3
rbasa <=>
2023-09-20 13:24:25 -0300

    coment code

--- src/routes/validatorRoutes.js
+++ src/routes/validatorRoutes.js
@@ -3,8 +3,11 @@ const router = express.Router();
 const validatorController = require('../controllers/validatorController');
 const userLoggedAsCategory = require('../middlewares/userLoggedAsCategory');
 
+// Validator home page
 router.get('/home/:api?', userLoggedAsCategory('validator'), validatorController.home);
+// Minting Request Validation
 router.put('/vote/:vote/:minting_request_id/:api?', userLoggedAsCategory('validator'), validatorController.validate);
+// Validator don't wants to take a case no analyse
 router.put('/cancelAssigment/:minting_request_id/:api?', userLoggedAsCategory('validator'), validatorController.cancelAssigment);
 
 
